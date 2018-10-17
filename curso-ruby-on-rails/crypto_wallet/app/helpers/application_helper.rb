6eea4853f84407e43c78d5c72afb479b43aeb780
saulo-lir <saulo.l.nascimento@hotmail.com>
2018-10-17 16:43:41 -0300

    Adicionado welcome_controller e alguns helpers

--- curso-ruby-on-rails/crypto_wallet/app/helpers/application_helper.rb
+++ curso-ruby-on-rails/crypto_wallet/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+    # Aqui nós definimos todos os métodos que serão utilizados como helpers
+    # Todos os helpers definidos nessa classe vão funcionar em toda a aplicação
+
+    def data_br(data_us)
+        data_us.strftime("%d/%m/%Y")
+    end
+
+    def nome_aplicacao
+        "CRYPTO_WALLET APP"
+    end
 end
